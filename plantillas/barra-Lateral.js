d2d449beb92ce6fd04ff91d783d565b8db33fcbf
Enrique Esteche <Enrique Esteche@EJE-ACER>
2020-11-03 10:35:37 -0300

    Primeros cambios con JavaScript

---
+++ plantillas/barra-Lateral.js
@@ -0,0 +1,39 @@
+   document.write(' \
+   <header class="header text-center">	    \
+	    <h1 class="blog-name pt-lg-4 mb-0"><a href="index.html">Enrique Javier</a></h1>\
+        <!--Barra de navegacion de la Izquierda -->\
+	    <nav class="navbar navbar-expand-lg navbar-dark" >\
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">\
+			<span class="navbar-toggler-icon"></span>\
+			</button>\
+			<div id="navigation" class="collapse navbar-collapse flex-column" >\
+\
+				<!--/Perfil corto /-->\
+				<div class="profile-section pt-3 pt-lg-0">\
+				    <img class="profile-image mb-3 rounded-circle mx-auto" src="images/profile1.jpg" alt="image" >			\
+					<div class="bio mb-3">Soy programador full stack junior, pero estoy mas enfocado en el backend. Soy un joven con muchos sueños y aspiraciones y con muchas ganas de tener mis primeras experiencias profesionales.<a href="about.html"> Lee mas sobre mi</a></div>\
+\
+\
+					<!--//Redes Sociales-->\
+					<ul class="social-list list-inline py-3 mx-auto">\
+			            <li class="list-inline-item"><a href="https://www.linkedin.com/in/enrique-esteche-241718177/"><i class="fab fa-linkedin-in fa-fw"></i></a></li>\
+			            <li class="list-inline-item"><a href="https://github.com/Codeje12"><i class="fab fa-github-alt fa-fw"></i></a></li>\
+			            <li class="list-inline-item"><a href="https://www.facebook.com/javierenrii"><i class="fab fa-facebook fa-fw"></i></a></li>\
+			        </ul><!--//sociales-lista-->\
+			        <hr> \
+				</div><!--//profile-section-->\
+				<ul class="navbar-nav flex-column text-left">\
+					<li class="nav-item active">\
+					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Inicio<span class="sr-only">(current)</span></a>\
+					</li>\
+					<li class="nav-item">\
+					    <a class="nav-link" href="blog-post.html"><i class="fas fa-bookmark fa-fw mr-2"></i>Personales</a>\
+					</li>\
+					<li class="nav-item">\
+					    <a class="nav-link" href="about.html"><i class="fas fa-user fa-fw mr-2"></i>Sobre mi</a>\
+					</li>\
+				</ul>\
+			</div>\
+		</nav>\
+    </header>\
+    ');
